91f4d6ee269200d781b16f4850e53f7d7db18427
juanra <juanra@192.168.1.134>
2017-03-21 19:35:07 +0100

    first commit

---
+++ spring-boot-web-thymeleaf/src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.mkyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
